5305b67ab2327c3b0e7c4d37386abca0eb4627c5
GrantChristie <grantchristie0@gmail.com>
2017-03-06 18:47:02 +0000

    8 complete and examples for 1

--- GrantChristie.hs
+++ GrantChristie.hs
@@ -6,6 +6,21 @@ inlist :: [Int] -> Int -> Bool
 inlist [] n = False
 inlist (x:xs) n = x == n || inlist xs n
 
+examples1 = do 
+  print ("inlist [2,3,2,4,7,9] 7 == True")
+  print (inlist [2,3,2,4,7,9] 7)
+  print ("inlist [2..100] 101 == False")
+  print (inlist [2..100] 101)
+  print ("inlist [2..] 101 == True")
+  print (inlist [2..] 101)
+
+--2. Write a function exactlyonce which takes a list of Integers and an Integer n,
+-- then returns a Boolean indicating whether n occurs exactly once in the list.
+
+exactlyonce :: [Int] -> Int -> Bool
+exactlyonce [] n = False
+exactlyonce xs n = length(filter (==n) xs) == 1
+
 --4. Write a function replace which takes an integer x and a list of integers. 
 --It returns a list where each element y of the list equals (x-y)*(x-y). 
 
@@ -19,4 +34,12 @@ replace x (y:ys) = f y : replace x ys where f y = (x-y)*(x-y)
 addthemup :: [[Int]] -> Int
 
 addthemup list = foldr (*) 1 (map sum list)
+
+--8. Define a function sequenceones such that sequenceones xs = True if and only if xs contains the substring 11.
+
+sequenceones :: [Int] -> Bool
+
+sequenceones (1:1:_) = True
+sequenceones (x:xs) = sequenceones xs
+sequenceones _ = False
 
